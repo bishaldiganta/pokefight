d253386f6f24f2bf90778f95580cac12930b9102
Pia <pia.koester@hey.com>
2023-11-29 16:00:54 +0100

    add background to card images, include HP in cards

--- client/src/components/PokemonCard.jsx
+++ client/src/components/PokemonCard.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const PokemonCard = ({pokemon}) => {
+const PokemonCard = ({ pokemon }) => {
   return (
-    <div className='rounded-md border-4 border-solid border-amber-300'>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name.english}/>
+    <div className="rounded-md border-4 border-solid border-amber-300">
+      <div>
         <h2>{pokemon.name.english}</h2>
-        <div></div>
-        <div></div>
-    </div>
-  )
-}
+        <p>{pokemon.base.HP}</p>
+        {/* <img src="" alt="" />  include the type icon*/}
+      </div>
+      <img
+        className="border-solid border-4  bg-slate-600	"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+        alt={pokemon.name.english}
+      />
 
-export default PokemonCard
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default PokemonCard;
