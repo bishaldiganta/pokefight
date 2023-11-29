a0374e0e8c875210558cf4693d28d4388459200f
Pia <pia.koester@hey.com>
2023-11-29 17:11:41 +0100

    style: card style with box shadow and centered content

--- client/src/components/PokemonCard.jsx
+++ client/src/components/PokemonCard.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const PokemonCard = ({ pokemon }) => {
   return (
-    <div className="rounded-md border-4 border-solid border-amber-300">
-      <div>
+    <div className="bg-white shadow-lg rounded-md overflow-hidden max-w-xs order-first lg:order-none border-8 border-solid border-amber-300 py-10 px-8">
+      <div className="flex justify-end gap-2">
         <h2>{pokemon.name.english}</h2>
         <p>{pokemon.base.HP}</p>
         {/* <img src="" alt="" />  include the type icon*/}
       </div>
       <img
-        className="border-solid border-4  bg-slate-600	"
+        className="w-full h-40 sm:h-48 object-cover bg-slate-400"
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
         alt={pokemon.name.english}
       />
