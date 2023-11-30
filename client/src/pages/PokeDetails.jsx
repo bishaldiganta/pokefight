7a73efc0d557917a13949cbfd4beb02a014e885d
ReallyBoard123 <chirag.prakriya@gmail.com>
2023-11-30 12:04:49 +0100

    added dynamic route and type based bg

--- client/src/pages/PokeDetails.jsx
+++ client/src/pages/PokeDetails.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 const PokeDetails = () => {
-  const pokemon = useLoaderData();
-  console.log(pokemon);
-  return (
-    <div>
-      more information about the pokemon from the pokeapi and not the json
-    </div>
-  );
+	const pokemon = useLoaderData();
+	console.log(pokemon);
+	return <div>{pokemon.name}, I choose you!</div>;
 };
 
 export default PokeDetails;
