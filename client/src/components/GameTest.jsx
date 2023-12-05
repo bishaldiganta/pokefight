ee8ce2054c93c0d58f54de81806d0f29735f1b73
Pia <pia.koester@hey.com>
2023-12-05 20:09:47 +0100

    add computer attack and damage logic

--- client/src/components/GameTest.jsx
+++ client/src/components/GameTest.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import pokeArray from "../game/pokearray";
 import { getpokeCard } from "../api/tcgapi";
-import clsx from "clsx";
+import WinningPage from "./WinningPage";
 
 const GameTest = () => {
   const [computerCard, setComputerCard] = useState([]);
   const [playerCard, setPlayerCard] = useState(null);
 
   const [computerHP, setComputerHP] = useState(0);
-  const [playerHP, setPlayerHP] = useState();
+  const [playerHP, setPlayerHP] = useState(0);
 
   useEffect(() => {
     assignCards();
@@ -43,31 +43,52 @@ const GameTest = () => {
   //   : console.log(playerCard, computerCard);
 
   const playerDamage = (e) => {
-    console.log(e.target.value);
     const attack = e.target.value;
     const damage = +playerCard.attacks
       .find((element) => {
         return element.name === attack;
       })
       .damage.replace(/\D/g, "");
-    console.log("the damage is", damage);
     setComputerHP((prev) => {
+      if (prev - damage <= 0) return "you won";
       return prev - damage;
     });
     console.log(computerHP);
+    setTimeout(computerDamage, 2000);
   };
-
-  // const newValue = value - damage;
   const barWidth = (computerHP / computerCard?.hp) * 100;
-  // const hitWidth = (damage / value) * 100 + "%";
-  console.log(barWidth);
 
-  return (
+  const computerDamage = () => {
+    const attackAmount = computerCard.attacks.length;
+    const attackIndex = Math.floor(Math.random() * attackAmount);
+    console.log(
+      "the length of the array is:",
+      attackAmount,
+      "the random number is",
+      attackIndex
+    );
+    const damage = +computerCard.attacks[attackIndex].damage.replace(/\D/g, "");
+
+    console.log("computer Damage", damage);
+    setPlayerHP((prev) => {
+      if (prev - damage <= 0) return "you lose";
+      return prev - damage;
+    });
+    console.log(playerHP);
+  };
+  const playerBarWidth = (playerHP / playerCard?.hp) * 100;
+
+  return computerHP === "you won" ? (
+    <WinningPage />
+  ) : playerHP === "you lose" ? (
+    "you are a looser"
+  ) : (
     <div>
       <div className="computer-card">
         <div className="flex justify-center">
           <img src={computerCard?.images?.large} className="m-2 w-2/6" />
         </div>
+
         <div className="flex justify-center">
           <p>{computerHP}</p>
           <div
@@ -109,10 +130,13 @@ const GameTest = () => {
             data-total="1000"
             data-value="1000"
           >
-            <div className="bar bg-green-500 w-full h-2.5 relative"></div>
+            <div
+              className="bar bg-green-500 h-2.5 relative"
+              style={{ width: `${playerBarWidth}%` }}
+            ></div>
             <div className="hit bg-white bg-opacity-60 absolute top-0 right-0 bottom-0 w-0"></div>
           </div>
-          <p>{playerCard?.hp}</p>
+          <p>{playerHP}</p>
         </div>
         <div className="flex justify-center">
           <img src={playerCard?.images?.large} className="m-2 w-2/6" />
---
+++ client/src/components/WinningPage.jsx
@@ -0,0 +1,11 @@
+export default function WinningPage() {
+  return (
+    <>
+      <h2>YOU WON</h2>
+      <img
+        src="https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt=""
+      />
+    </>
+  );
+}
