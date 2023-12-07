c5d442add24c9c555b5e63b835022df3f1b2a845
Pia <pia.koester@hey.com>
2023-12-07 18:00:28 +0100

    add evolution information in Pokemon Details

--- client/src/pages/PokeDetails.jsx
+++ client/src/pages/PokeDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const PokeDetails = () => {
   const pokemon = useLoaderData();
@@ -8,23 +8,34 @@ const PokeDetails = () => {
   const pokeData = pokemon.data;
   return (
     <>
-    <div>
-      <h2 className="text-2xl font-bold text-center">{pokeData[0].name}</h2>
-      <p className="text-center">Information about pokemon</p>
+      <div>
+        <h2 className="text-2xl font-bold text-center">{pokeData[0].name}</h2>
+        <p className="text-center">{pokeData[0].flavorText}</p>{" "}
+        <p className="text-center">
+          {" "}
+          {pokeData[0].evolvesFrom
+            ? `Evolves from: ${pokeData[0].evolvesFrom}`
+            : ""}
+        </p>
+        <p className="text-center">
+          {pokeData[0].evolvesTo ? `Evolves to: ${pokeData[0].evolvesTo}` : ""}
+        </p>
       </div>
       <div className="flex justify-center">
         <div className="rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-40 p-10">
           {pokeData.map((pokemon) => {
             return (
-              <motion.div 
+              <motion.div
                 whileHover={{ scale: 1.1 }}
                 className="shadow-lg overflow-hidden rounded-lg max-w-xs min-w-[250px] w-full mx-auto"
-                key={pokemon.id}>
-                <img 
+                key={pokemon.id}
+              >
+                <img
                   className="w-full h-auto"
                   src={pokemon.images.large}
                   alt={`${pokemon.name} card`}
-                /> {/* TO-DO- ADD COLORED BACKGROUND SHADOW TO CARDS BASED ON typeColors. Also add card flip animation. Check MIRO. */}
+                />{" "}
+                {/* TO-DO- ADD COLORED BACKGROUND SHADOW TO CARDS BASED ON typeColors. Also add card flip animation. Check MIRO. */}
               </motion.div>
             );
           })}
