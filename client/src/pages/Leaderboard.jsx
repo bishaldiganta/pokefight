6350c48284c59b7c7dc22c973138db42662ca6db
bishaldiganta <bishal.diganta@gmail.com>
2023-12-08 15:09:42 +0100

    add styling to leaderboard

--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -3,32 +3,30 @@ import { useLoaderData } from "react-router-dom";
 const Leaderboard = () => {
   let pokemons = useLoaderData();
 
-  console.log(pokemons);
-
   return (
-    <div className="flex items-center flex-col ">
-      <h1>Leaderboard</h1>
-      <table>
+    <div className="flex items-center flex-col bg-yellow-200 rounded-xl p-4">
+      <h1 className="text-3xl font-bold text-gray-800 mb-6">Leaderboard</h1>
+      <table className="table-auto text-lg text-gray-800">
         <thead>
-          <tr>
-            <th>Pokemon Name</th>
-            <th>Score</th>
+          <tr className="bg-yellow-200 rounded-xl">
+            <th className="p-2">Pokemon</th>
+            <th className="p-2">Score</th>
           </tr>
         </thead>
         <tbody>
           {pokemons.map((pokemon) => {
             if (pokemon.score) {
               return (
-                <tr key={pokemon.name.english}>
-                  <td>
+                <tr key={pokemon.name.english} className="text-center">
+                  <td className="p-3 flex items-center justify-center">
                     <img
-                      className="w-20 object-cover"
+                      className="w-20 h-20 object-cover rounded-full mr-2"
                       src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`}
                       alt={pokemon.name.english}
                     />
+                    {pokemon.name.english}
                   </td>
-                  <td>{pokemon.name.english}</td>
-                  <td>{pokemon.score}</td>
+                  <td className="p-3">{pokemon.score}</td>
                 </tr>
               );
             }
